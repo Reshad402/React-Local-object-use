14fafac331c8850bdebab2efb0c63871e81860a1
Reshad402 <mdrhr402@gmail.com>
2022-10-29 18:58:43 +0600

    Uniqe Key add kora

--- src/Utilities/Cosmetics/Cosmetics.js
+++ src/Utilities/Cosmetics/Cosmetics.js
@@ -2,19 +2,23 @@ import React from 'react';
 
     const Cosmetics = () => {
         const cosmetics = [
-            {id:1,name:'blic',price:100},
-            {id:2,name:'clic',price:200},
-            {id:3,name:'dlic',price:300},
-            {id:4,name:'elic',price:400},
-            {id:5,name:'flic',price:500}
+            {id:1,name:'Apple',price:150},
+            {id:2,name:'Orange',price:200},
+            {id:3,name:'Duck',price:300},
+            {id:4,name:'Hen',price:400},
+            {id:5,name:'Beef',price:800}
         ]
         
     return (
         <div>
             <h1>Welcome to the cosmetic store</h1>
            {
-            cosmetics.map((cosmetic) => console.log(cosmetic))
-                
+                     // cosmetics.map((cosmetic) => console.log(cosmetic))
+                cosmetics.map((cosmetic) => 
+                    <Cosmetics key={cosmetic.id}
+                    // !Every Cosmetic er modde akta id ase 
+                    ></Cosmetics>)
+
            }
 
         </div>
