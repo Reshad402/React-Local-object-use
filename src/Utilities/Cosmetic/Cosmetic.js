f4a9d206ef268e509692e7193e430efae3309e20
Reshad402 <mdrhr402@gmail.com>
2022-10-30 00:15:36 +0600

    Add button-with-Show it in console

--- src/Utilities/Cosmetic/Cosmetic.js
+++ src/Utilities/Cosmetic/Cosmetic.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 const Cosmetic = (props) => {
     const {name,price,id} = props.singleObject;
+
+    const addToCart = (id) =>{
+        console.log('Item added',id)
+    }
+    const addToName = (name) =>{
+        console.log('Item added',name)
+    }
     return (
         <div style={{border:'2px solid yellow',margin:'10px',borderRadius:'10px'}}>
             <h2>Buy this :{name}</h2>
             <p>Price is : {price}</p>
             <h4>The id is: {id}</h4>
+            <button onClick={() =>addToCart(id)}>Add to Cart</button>
+            <button onClick={() => addToName(name)}>Add the name</button>
+            {/* //! Event handeler a kono kisu id or nam add korte chaile avabe korte lagbe */}
         </div>
     );
 };
