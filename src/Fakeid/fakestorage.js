69a722d6f1e4134d23bee00c9c4718711773902d
Reshad402 <mdrhr402@gmail.com>
2022-10-30 20:28:11 +0600

    Local storage best option

--- src/Fakeid/fakestorage.js
+++ src/Fakeid/fakestorage.js
@@ -1,4 +1,27 @@
 const addToLS = id =>{
-    localStorage.setItem(id,1)
+
+    let shoppingCart ;
+
+        const storedCart = localStorage.getItem('shoppingCart');
+
+            if(storedCart){
+                shoppingCart = JSON.parse(storedCart);
+            }
+            else{
+                shoppingCart = {};
+            }
+
+
+    const quantity = shoppingCart[id];
+    if(quantity){
+        
+        const newQuantity = quantity+1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart))
 }
-export {addToLS}
+export {addToLS};
