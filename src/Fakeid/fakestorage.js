ee76296ef711fc5fe06e11ebcc3492ef69393bbd
Reshad402 <mdrhr402@gmail.com>
2022-10-31 08:33:08 +0600

    adding remove item

--- src/Fakeid/fakestorage.js
+++ src/Fakeid/fakestorage.js
@@ -1,17 +1,23 @@
 const addToLS = id =>{
+// ------------------------------------------------------------------------------------------------------------
+    let shoppingCart = {};  //! Declaring a empty objects
 
-    let shoppingCart ;
+        const storedCart = localStorage.getItem('shoppingCart'); //! Getting the value from localStorage
 
-        const storedCart = localStorage.getItem('shoppingCart');
-
+                        //! Get the shopping cart from localStorage 
             if(storedCart){
-                shoppingCart = JSON.parse(storedCart);
+                shoppingCart = JSON.parse(storedCart);  //? ata json theke js a ana hoiche
             }
-            else{
-                shoppingCart = {};
-            }
+            // ! It need not be used cz it is declared in first stage as let 
+            //?     else{
+            //?        shoppingCart = {};
+            //?    }
+// _________________________________________________________________________________________________________________________
+      
 
 
+
+//! Add quantity to shoppingCart
     const quantity = shoppingCart[id];
     if(quantity){
         
@@ -21,7 +27,25 @@ const addToLS = id =>{
     else{
         
         shoppingCart[id] = 1;
+        
     }
     localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart))
+    
 }
-export {addToLS};
+// ---------------------------------------Remove Item ----------------------------------------------------
+
+const removeItemDb = id =>{
+    // console.log('removeItemDb',id)
+    const storedCart =localStorage.getItem('shoppingCart');     //! Checking if the stored item is exists
+    if(storedCart){
+        const shoppingCart = JSON.parse(storedCart);
+        if(id in shoppingCart){
+            // console.log('Exits')
+            delete shoppingCart[id];
+                        localStorage.setItem('shoppingCart',JSON.stringify(shoppingCart))
+        }
+
+    }
+}
+
+export {addToLS,removeItemDb};
--- src/Utilities/Cosmetic/Cosmetic.js
+++ src/Utilities/Cosmetic/Cosmetic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addToLS } from '../../Fakeid/fakestorage';
+import { addToLS, removeItemDb } from '../../Fakeid/fakestorage';
 
 
 
@@ -8,13 +8,18 @@ const Cosmetic = (props) => {
 
     const addToCart = (id) =>{
         // console.log('Item added',id)
-        addToLS(id)
+        addToLS(id)                     //! Declaring a function from fakestorage to add to the cart;
         
     }
     const addToName = (name) =>{
-        console.log('Item added',name)
+        console.log('Item added',name)  //! Declaring a function from fakestorage to store name;
         
     }
+
+    const removeItem = (id) =>{
+        removeItemDb(id)                //! Declaring a function from fakestorage to remove;
+    }
+
     return (
         <div style={{border:'2px solid yellow',margin:'10px',borderRadius:'10px'}}>
             <h2>Buy this :{name}</h2>
@@ -22,6 +27,8 @@ const Cosmetic = (props) => {
             <h4>The id is: {id}</h4>
             <button onClick={() =>addToCart(id)}>Add to Cart</button>
             <button onClick={() => addToName(name)}>Add the name</button>
+            <button onClick={() => removeItem(id)}>Remove-Item</button>
+
             {/* //! Event handeler a kono kisu id or nam add korte chaile avabe korte lagbe */}
         </div>
     );
